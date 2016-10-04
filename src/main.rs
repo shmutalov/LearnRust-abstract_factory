68b24845bc80eb33483ad9e15e18eb8af5ababaa
shmutalov <shmutalov@gmail.com>
2016-10-04 10:22:51 +0500

    Print string value, not string itself )

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ use protocols::protocol_factory::ProtocolFactory;
 
 fn main() {
     let proto = ProtocolFactory::get_protocol(ProtocolTypes::Version1);
-    println!("{:?}", proto.get_name());
+    println!("{}", proto.get_name());
 
     let proto = ProtocolFactory::get_protocol(ProtocolTypes::Version2);
-    println!("{:?}", proto.get_name());
+    println!("{}", proto.get_name());
 }
